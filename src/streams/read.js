7e5d2c0e4729eb53fb0a08ba8933f870f1b80c79
Arthur <Frenki_art@mail.ru>
2024-10-02 22:34:00 +0300

    feat: function read strim implemented

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import fs from "fs";
+import path from "path";
+
 const read = async () => {
-    // Write your code here 
+  const targetPath = path.join(import.meta.dirname, "files", "fileToRead.txt");
+  const readableStream = fs.createReadStream(targetPath);
+
+  readableStream.on("data", (chunk) => {
+    console.log(chunk.toString());
+  });
 };
 
 await read();
