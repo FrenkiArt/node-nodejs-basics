f51409fb70540dc511c5307384be43ecc12e91d9
Arthur <Frenki_art@mail.ru>
2024-10-02 11:35:17 +0300

    feat: delete functions is implemented

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,40 @@
+import fs from "fs";
+import path from "path";
+import throwError from "./throw_error.js";
+import throwSuccess from "./throw_success.js";
+
+/* 
+TODO: 
+delete.js - implement function that deletes file fileToRemove.txt 
+(if there's no file fileToRemove.txt Error with message FS operation failed must be thrown)
+*/
+
+const dto = {
+  targetFileName: "fileToRemove.txt",
+  targetDirName: "files",
+  currentDirPath: path.join(import.meta.dirname),
+};
+
 const remove = async () => {
-    // Write your code here 
+  // проверка на существование файла fileToRemove.txt
+  fs.access(
+    path.join(dto.currentDirPath, dto.targetDirName, dto.targetFileName),
+    fs.constants.F_OK,
+    targetFileRemoveHandler
+  );
+};
+
+const targetFileRemoveHandler = (err) => {
+  fs.unlink(
+    path.join(dto.currentDirPath, dto.targetDirName, dto.targetFileName),
+    (err) => {
+      if (err) {
+        throwError();
+      } else {
+        throwSuccess("всё  чики пуки, файл удалён...");
+      }
+    }
+  );
 };
 
 await remove();
