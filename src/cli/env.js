3bdcc59f1c6eb955df9af974da71b7732d81d6fd
Arthur <Frenki_art@mail.ru>
2024-10-02 13:28:25 +0300

    feat: env.js is implemented

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,21 @@
+/* 
+TODO:
+env.js - implement function that parses environment variables with prefix RSS_ 
+and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
+*/
+
 const parseEnv = () => {
-    // Write your code here 
+  //console.log(process.env);
+
+  for (const key in process.env) {
+    if (key.startsWith("RSS_")) {
+      printRssEnv(key, process.env[key]);
+    }
+  }
+};
+
+const printRssEnv = (val1, val2) => {
+  console.log(`${val1}=${val2};`);
 };
 
 parseEnv();
