89adc3b4216eea56e17346d1f67f3d50dd3709f9
Arthur <Frenki_art@mail.ru>
2024-10-02 12:51:38 +0300

    refactor

--- src/fs/throw_error.js
+++ src/fs/throw_error.js
@@ -1,5 +1,9 @@
-const throwError = () => {
-  throw new Error(`FS operation failed`);
+const throwError = (err) => {
+  if (!err) {
+    throw new Error(`FS operation failed`);
+  }
+
+  throw new Error(err);
 };
 
 export default throwError;
