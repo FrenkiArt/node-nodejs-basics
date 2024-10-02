ee78b18f4224bf2ada6a99ac8336d5b8de629f78
Arthur <Frenki_art@mail.ru>
2024-10-02 13:41:11 +0300

    feat: args functions is implemented

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,27 @@
+/* 
+TODO:
+args.js - implement function that parses command line arguments 
+(given in format --propName value --prop2Name value2, you don't need to validate it) 
+and prints them to the console in the format propName is value, prop2Name is value2
+*/
+
 const parseArgs = () => {
-    // Write your code here 
+  console.log(process.argv);
+  // пропускаем первые два, потому что там путь к node.js и путь к самому файлу
+  const args = process.argv.slice(2);
+
+  args.forEach((arg, i) => {
+    if (arg.startsWith("--")) {
+      const val1 = arg.slice(2);
+      const val2 = args[i + 1];
+
+      printArgs(val1, val2);
+    }
+  });
+};
+
+const printArgs = (val1, val2) => {
+  console.log(`${val1} is ${val2},`);
 };
 
 parseArgs();
