29629a137990f043eae95d99810f39d4230d29a1
Arthur <Frenki_art@mail.ru>
2024-10-03 12:50:34 +0300

    refactor: read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -9,20 +9,31 @@ process.stdout
 */
 
 const read = async () => {
-  //process.stdout.setEncoding("utf8");
   const targetPath = path.join(import.meta.dirname, "files", "fileToRead.txt");
   const readableStream = fs.createReadStream(targetPath);
 
-  readableStream.on("data", (chunk) => {
+  readableStream.setEncoding("utf8");
+
+  /* readableStream.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
+
+  readableStream.on("end", () => {
+    process.stdout.end();
+  }); */
+
+  /* readableStream.on("data", (chunk) => {
     process.stdout.write(chunk.toString());
 
     //console.log(chunk.toString());
-  });
+  }); */
+
+  //fs.createReadStream(targetPath).pipe(process.stdout);
 
   // не понятно, если прописать "node .\src\streams\read.js",
-  // то выведет в консоль
+  // то выведет в консоль нормально
 
-  //readableStream.pipe(process.stdout);
+  readableStream.pipe(process.stdout);
 };
 
 await read();
