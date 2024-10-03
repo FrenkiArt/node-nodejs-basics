2ba090309bafc69d214f351cf518000c0ca02410
Arthur <Frenki_art@mail.ru>
2024-10-03 12:02:58 +0300

    feat: write function implemented

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,13 +1,28 @@
 import fs from "fs";
 import path from "path";
 
+/* 
+TODO:
+read.js - implement function that reads file fileToRead.txt 
+content using Readable Stream and prints it's content into 
+process.stdout
+*/
+
 const read = async () => {
+  //process.stdout.setEncoding("utf8");
   const targetPath = path.join(import.meta.dirname, "files", "fileToRead.txt");
   const readableStream = fs.createReadStream(targetPath);
 
   readableStream.on("data", (chunk) => {
-    console.log(chunk.toString());
+    process.stdout.write(chunk.toString());
+
+    //console.log(chunk.toString());
   });
+
+  // не понятно, если прописать "node .\src\streams\read.js",
+  // то выведет в консоль
+
+  //readableStream.pipe(process.stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,28 @@
+import fs from "fs";
+import path from "path";
+
+/* 
+TODO:
+write.js - implement function that writes process.stdin 
+data into file fileToWrite.txt content using Writable Stream
+*/
+
 const write = async () => {
-    // Write your code here 
+  const targetPath = path.join(import.meta.dirname, "files", "fileToWrite.txt");
+
+  const writeableStream = fs.createWriteStream(targetPath);
+
+  /* process.stdin.on("data", (chunk) => {
+    writeableStream.write(chunk);
+
+    if (chunk.includes("stop") || chunk.includes("стоп")) {
+      writeableStream.end();
+      process.exit();
+    }
+  }); */
+
+  process.stdin.pipe(writeableStream);
+  // встаёт вопрос, а как закрыть поток в случае передачи через pipe?
 };
 
 await write();
