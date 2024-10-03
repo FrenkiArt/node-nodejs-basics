0c3fa1a7a6bfd598018b84c85d9f11286a7cf6ef
Arthur <Frenki_art@mail.ru>
2024-10-03 14:02:39 +0300

    feat: hash function implemented

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,46 @@
+import stream from "stream";
+import { createHmac } from "crypto";
+import path from "path";
+import fs from "fs";
+
+/* 
+TODO:
+calcHash.js - implement function that calculates SHA256 hash 
+for file fileToCalculateHashFor.txt and logs it into console 
+as hex using Streams API
+*/
+
+class hashTransform extends stream.Transform {
+  constructor() {
+    super();
+  }
+
+  _transform(chunk, encoding, callback) {
+    this.push(createHmac("sha256", secret).update(chunk).digest("hex"));
+
+    if (callback) callback();
+  }
+}
+
+// взято из документации https://nodejs.org/api/crypto.html
+const secret = "abcdefg";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const targetPath = path.join(
+    import.meta.dirname,
+    "files",
+    "fileToCalculateHashFor.txt"
+  );
+
+  const readableStream = fs.createReadStream(targetPath);
+  const hashStream = new hashTransform();
+
+  readableStream.setEncoding("utf8");
+
+  readableStream.pipe(hashStream).pipe(process.stdout);
+
+  // не понимаю, вот так работает node .\src\hash\calcHash.js
+  // при написании npm run hash , на сегудну показывает и скрывает
 };
 
 await calculateHash();
