6baf0d4f7e4c6bff3a1e8bb54e7f4b83350c3c27
Arthur <Frenki_art@mail.ru>
2024-10-06 00:39:09 +0300

    feat: workers functions implemented

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,63 @@
+import { Worker, isMainThread } from "worker_threads";
+import path from "path";
+import os from "os";
+
+/* 
+TODO:
+main.js - implement function that creates number of worker threads 
+(equal to the number of host machine logical CPU cores) 
+from file and able to send data to those threads and to receive 
+result of the computation from them. You should send incremental 
+number starting from to each . For example: on host machine 
+with 4 cores you should create 4 workers and send 10 to first , 
+11 to second , 12 to third , 13 to fourth . 
+After all workers will finish, function should log array of results 
+into console. The results are array of objects with 2 properties: 
+worker.js10workerworkerworkerworkerworker
+status - 'resolved' in case of successfully received value 
+from or in case of error in worker'error'worker
+data - value from in case of success or in case of error 
+in workerworkernull
+*/
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpuInfo = os.cpus();
+  const cpuCount = cpuInfo.length;
+
+  //console.log(cpuCount);
+
+  // Создаем потоки
+  if (isMainThread) {
+    const worker = new Worker(path.join(import.meta.dirname, "worker.js"));
+
+    // Создаем массив результатов
+    const results = [];
+    let startCount = 9;
+
+    // Передача данных в рабочий поток и получение результатов
+    for (let i = 0; i <= cpuCount; ++i) {
+      worker.postMessage(startCount); // Отправляем данные в рабочий поток
+
+      let result = {};
+
+      try {
+        worker.on("message", (data) => {
+          result.status = "resolved";
+          result.data = data;
+
+          results.push(result);
+          console.log(results);
+
+          worker.terminate();
+        });
+      } catch (error) {
+        result.status = "error";
+        result.data = null;
+      }
+
+      startCount++;
+    }
+  }
 };
 
 await performCalculations();
